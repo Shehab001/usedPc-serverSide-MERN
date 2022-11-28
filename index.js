006b1e8a9be940104837c18577df58f74c0d0f7d
Md. Shehab Chowdhury <shehab15-11396@diu.edu.bd>
2022-11-28 13:15:35 +0600

    advertise  done

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 
 const port = process.env.PORT || 5000;
@@ -136,15 +136,25 @@ async function run() {
     });
     app.get("/sellerproduct/:id", async (req, res) => {
       const email = req.params.id;
-      console.log(email);
+      //console.log(email);
       const query = { email: email };
       const result = await productdetails.find(query).toArray();
       res.send(result);
     });
     app.post("/addproduct", async (req, res) => {
       const data = req.body;
-      console.log(data);
+      //console.log(data);
       const result = await productdetails.insertOne(data);
+      res.send(result);
+    });
+
+    app.get("/sadvertise/:id", async (req, res) => {
+      const id = req.params.id;
+      console.log(id);
+      const query = { _id: ObjectId(id) };
+      const update = { $set: { advertise: "Yes" } };
+      const options = { upsert: true };
+      const result = await productdetails.updateOne(query, update, options);
       res.send(result);
     });
   } finally {
